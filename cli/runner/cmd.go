a4f4163ba55216faaed649b4908df406734b02f4
Eugene Sylenko <1863154+ffenix113@users.noreply.github.com>
2024-02-03 01:32:23 +0100

    Allow building firmware with CLI
    For this to work two environment variables must be defined:
    
    * NCS_TOOLCHAIN_BASE
    * ZEPHYR_BASE
    
    NCS_TOOLCHAIN_BASE will be in form like
    "/home/<user>/ncs/toolchains/7795df4459"
    (where last part is some ID of the NCS instance),
    ZEPHYR_BASE will be in form like
    "/home/<zephyr>/ncs/zephyr"

--- cli/runner/cmd.go
+++ cli/runner/cmd.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
+	"strings"
 )
 
 type Cmd struct {
@@ -34,17 +36,83 @@ func (c *Cmd) AddArg(arg string, value ...string) *Cmd {
 }
 
 func (c *Cmd) Run(ctx context.Context, workDir string) error {
+	ncsToolchainPath := os.Getenv("NCS_TOOLCHAIN_BASE")
+	zephyrPath := os.Getenv("ZEPHYR_BASE")
+
+	var extendedEnv []string
+	if ncsToolchainPath != "" && zephyrPath != "" {
+		extendedEnv = c.extendEnv(ncsToolchainPath, zephyrPath)
+
+		newPath := extendedEnv[0]
+		pathParts := strings.SplitN(newPath, "=", 2)
+		os.Setenv("PATH", pathParts[1])
+
+		commandPath, err := exec.LookPath(c.command)
+		if err != nil {
+			return err
+		}
+
+		c.command = commandPath
+	}
+
 	cmd := exec.CommandContext(ctx, c.command, c.args...)
 
 	cmd.Dir = workDir
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.Env = append(cmd.Env, os.Environ()...)
+	cmd.Env = append(extendedEnv, append(cmd.Env, os.Environ()...)...)
 
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("run command %q: %w", c.command, err)
 	}
 
 	return nil
+}
+
+func (c *Cmd) extendEnv(ncsToolchainPath string, zephyrPath string) []string {
+	ncsCombinedPath := generateEnvArray(ncsToolchainPath, []string{
+		"/usr/bin",
+		"/usr/local/bin",
+		"/opt/bin",
+		"/opt/nanopb/generator-bin",
+		"/opt/zephyr-sdk/aarch64-zephyr-elf/bin",
+		"/opt/zephyr-sdk/x86_64-zephyr-elf/bin",
+		"/opt/zephyr-sdk/arm-zephyr-eabi/bin",
+	})
+
+	envPath := os.Getenv("PATH")
+	combinedPath := ncsCombinedPath
+	if envPath != "" {
+		combinedPath += ":" + envPath
+	}
+
+	pythonPath := generateEnvArray(ncsToolchainPath, []string{
+		"/usr/local/lib/python3.8",
+		"/usr/local/lib/python3.8/site-packages",
+	})
+
+	ldLibraryPath := generateEnvArray(ncsCombinedPath, []string{
+		"/usr/lib",
+		"/usr/lib/x86_64-linux-gnu",
+		"/usr/local/lib",
+	})
+
+	return []string{
+		"PATH=" + combinedPath,
+		"ZEPHYR_BASE=" + zephyrPath,
+		"ZEPHYR_SDK_INSTALL_DIR=" + path.Join(ncsToolchainPath, "/opt/zephyr-sdk"),
+		"ZEPHYR_TOOLCHAIN_VARIANT=zephyr",
+		"PYTHOHOME=" + path.Join(ncsCombinedPath, "/usr/local"),
+		"PYTHONPATH=" + pythonPath,
+		"LD_LIBRARY_PATH=" + ldLibraryPath,
+	}
+}
+
+func generateEnvArray(prefix string, vals []string) string {
+	for i := range vals {
+		vals[i] = path.Join(prefix, vals[i])
+	}
+
+	return strings.Join(vals, ":")
 }
