b8ae83b33c2085254fa2c59db0bb22b270a2338d
ffenix113 <1863154+ffenix113@users.noreply.github.com>
2025-11-07 17:47:55 +0100

    fix: on_off template cpp syntax

--- templates/src/types_on_off.hpp
+++ templates/src/types_on_off.hpp
@@ -13,11 +13,11 @@ namespace zbhome
         class OnOff : public zbhome::types::ZCLCommandHandler
         {
         public:
-            OnOff(const struct gpio_dt_spec pin) : m_pin(pin);
+            OnOff(const struct gpio_dt_spec pin) : m_pin(pin) {};
             // This function will be called when callback is received with the endpoint
             // that this components is for. Otherwise cluster/attribute id's are not
             // checked and are up to the implementation to validate.
-            void zclSetAttrValue(zb_zcl_set_attr_value_param_t* setAttrValueParam);
+            void zclSetAttrValue(zb_zcl_set_attr_value_param_t *setAttrValueParam);
 
         private:
             const struct gpio_dt_spec m_pin = {};
